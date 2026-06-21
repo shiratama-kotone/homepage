345d30f89c209aed7e2f36de75783d6015db91a5
ゆゆゆ <yuyuyu1226.m.1226@gmail.com>
2026-06-21 13:24:24 -0300

    style.css,script.jsの追加

---
+++ script/script.js
@@ -0,0 +1,13 @@
+document.getElementById("header").innerHTML = `
+
+<header>
+  <h2>ゆゆゆのホームページ</h2>
+</header>
+`;
+
+document.getElementById("footer").innerHTML = `
+
+<footer>
+  <p>© ゆゆゆ</p>
+</footer>
+`;
