56b2f3a6dc490191bacf2842eb97afe49d874648
ゆゆゆ <yuyuyu1226.m.1226@gmail.com>
2026-07-22 15:18:56 +0900

    今更CHUNITHM Mateをやってきた

--- blog/posts.js
+++ blog/posts.js
@@ -5,5 +5,12 @@ const posts = [
     desc: "デザインとかいろいろ変えた",
     tags: ["日記", "ホームページ"],
     url: "/blog/1.html"
+  },
+  {
+    title: "今更CHUNITHM Mateをやってきた",
+    date: "2026-7-21",
+    desc: "ほんとに今更",
+    tags: ["CHUNITHM","CHUNITHM Mate"],
+    url: "/blog/2.html"
   }
 ];
